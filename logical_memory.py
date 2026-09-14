3dd5bac728bc26acf47dc83cd58e41a042128417
Addison <addison.bullock@ucas-edu.net>
2026-09-14 14:29:15 -0600

    forced push larose

---
+++ logical_memory.py
@@ -0,0 +1,31 @@
+# AB, Logical and psysical memory notes
+
+pi= 3.141592
+
+school= "UCAS"
+
+print(f"The variable pi has a value of{pi}and is saved at{id(pi)}")
+
+print(f"The variable school has a value of{schol}and is saved at{id(school)}")
+
+
+
+
+
+
+
+
+
+
+name= "Addison"
+age= "14"
+
+score= 10
+score= 10
+
+hight= 5.9
+city= "Mapleton"
+zip_code= 84664
+
+price1= 10
+price2= 10
---
+++ variables_practice.py
@@ -0,0 +1,40 @@
+#AB, Variables Practice
+name= "Addison"
+
+print(name)
+
+anumberbetween1and10= 5
+
+print(anumberbetween1and10)
+
+anumberbetween100and1000= 30
+
+print(anumberbetween100and1000)
+
+whatyouhadforbreakfast= "idk"
+
+print(whatyouhadforbreakfast)
+
+yourfavoritecolor= "blue"
+
+print(yourfavoritecolor)
+
+aschoolname= "ucas"
+
+print(aschoolname)
+
+theyear="sep"
+
+print(theyear)
+
+youreyecolor="brown"
+
+print(youreyecolor)
+
+someonesage=80
+
+print(someonesage)
+
+yourfavoritesubjectinschool="math"
+
+print(yourfavoritesubjectinschool)
