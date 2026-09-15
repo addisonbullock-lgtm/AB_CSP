b8f26baff67cd97bf65586f0223bef66915738b4
Addison <addison.bullock@ucas-edu.net>
2026-09-15 14:50:43 -0600

    hard push Larose

---
+++ fixing_intputs.py
@@ -0,0 +1,11 @@
+# AB Fixing user inputs
+
+Whiletrue: color=input("Tell me a color:").strip().lower()
+print("That is a number not a color")
+print(f"We painted the walls {color}!")elif"" in color print("I said one word.")
+else:
+    break
+print(f"We painted the walls {color}!")
+
+while True:
+    
