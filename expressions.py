4a93d200e2cf5d5503c28f13cdf18696ddb605b3
Addison <addison.bullock@ucas-edu.net>
2026-09-16 14:52:48 -0600

    hard push larose

---
+++ expressions.py
@@ -0,0 +1,31 @@
+# AB 7th Intergers, Floats, and Expressions notes
+
+# Integer=> a whole number
+students= 23
+cars= 50
+computors= 29
+awarness= -12
+
+# Float=> numbers with decimals
+pi= 3.1415
+temp= -95.6
+cost= 1.99
+rain= 2.17
++= addition -= subtract *= multiply /= division **= exponets %= modulo //= integer division
+
+#Arithmetic Operators (+-*/ ** // %)
+print(f"18/4 is {18/4} or {18//4} with a remainder of {18%4}")
+print(f"18/5 is {18/4} or {18//5} with a remainder of {18%5}")
+
+# order of operations
+grades= {85,66,94,72,88,79,100}
+students= len(grades)
+average= sum(grades)/students
+
+print(f"The average is {int(average)}")
+
+# Convert the data type
+price= input("Hownmuch did the item cost:")
+tax= 0.485
+sales_tax= price*tax
+total= price+sales_taxprint(f"Your total is {total}")
