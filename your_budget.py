c970ecc57bb6ac9df2220fe902314aff771cec2b
Addison <addison.bullock@ucas-edu.net>
2026-09-21 14:52:02 -0600

    SILLY_SENTENCES.PY

---
+++ your_budget.py
@@ -0,0 +1,22 @@
+# AB 7th Your_budget
+
+while True:
+    try:
+        income = float(input("What is your monthly income"))
+        break
+    exept:
+            print("You forgot to type your monthly income")
+
+            # while true loop
+
+            whle True:
+            Try:
+            utlities = float(input("What is your monthly ulitles"))
+            print("You forgot to add a number")
+
+            # While true loop
+
+            while True:
+                Try:
+                groceries = float(input("What is your monthly grocerice"))
+            
