217d996497f09a2631529ce0f57a7960b993c6f1
Addison <addison.bullock@ucas-edu.net>
2026-09-22 14:53:36 -0600

    message in the box

---
+++ Silly_sentences.py
@@ -0,0 +1,9 @@
+# AB Silly_sentences.py
+
+resturant=input("Tell me a type of resturant:")
+name=input= ("Tell me a boy name")
+bird=input= ("Tell me a type of bird")
+color=input= ("Tell me a color:")
+food=input=("Tell me a food item:")
+sun_glassesinput=("Tell me a type of sun_glasses:")
+noun=Input("Give me a story:")
