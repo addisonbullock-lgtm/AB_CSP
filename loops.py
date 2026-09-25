54de829807399980ee3e71defb7c6f307fd9b999
Addison <addison.bullock@ucas-edu.net>
2026-09-25 13:57:02 -0600

    hard push

---
+++ loops.py
@@ -0,0 +1,25 @@
+# AB, Loops Notes
+import random
+
+#1:startpoint
+#2:stp point boolean statement
+#3:increase iterater
+
+count = 1
+
+while count <= 10:
+    print(count)
+    count += 1
+
+ducks= 1
+goose= random.randint(1,11)
+while True:
+    if ducks == goose:
+        breaks
+    print("Duck....")
+    ducks += 1 #ducks= ducks + 1
+print("GOOSE!!!!")
+
+#Complex Data Type= holds other data in it
+siblings= ["Alex", "Katie", "Andrew", "Tia", "Treyson", "Xavier", "Jake"]
+peint(siblings[2])
